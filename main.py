4c45ba2c5a4f4c6c02ec9ade6d63435378708f36
sumanlokesh <sumanlokesh.dev@gmail.com>
2022-01-30 20:58:25 +0530

    No Change

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import server.server as servermod
-import client.client as clientmod
+from time import sleep
+from server.server import main as servserv
+from server.tracker import main as servtrack
+from client.client import main as clcl
+from client.server import main as clserv
+import threading
+from _thread import *
 
+
+thread_lock = threading.Lock()
 def main():
-    path = input("Welcome! Write the path of the file to be sent:")
+    start_new_thread(servserv, ())
+    sleep(1)
+    start_new_thread(servtrack, ())
+    sleep(1)
+    start_new_thread(clcl, ())
+    sleep(1)
+    start_new_thread(clcl, ())
+
     
 '''Implemented Tracker
 Modified server for new design
 updated chunker for better working'''
 
 
-
+if __name__ == '__main__':
+    main()
